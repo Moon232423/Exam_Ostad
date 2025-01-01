af125fedc7f52c798aeeb8f64d6be8e86ec459e8
Moon232423 <alaminmoon2003@gmail.com>
2025-01-01 23:13:58 +0600

    Updated

--- bin/project_c.dart
+++ bin/project_c.dart
@@ -3,18 +3,18 @@ import 'dart:io';
 void main() {
   triangle();
 }
-
 void triangle() {
-  print('Enter the base of the triangle: ');
+  print('Enter the base : ');
   double? base = double.tryParse(stdin.readLineSync()!);
+  print('Enter the height : ');
+    double? height = double.tryParse(stdin.readLineSync()!);
 
-  print('Enter the height of the triangle: ');
-  double? height = double.tryParse(stdin.readLineSync()!);
-
-  if (base != null && height != null) {
+    if (base != null && height != null)
+    {
     print('The area of the triangle is: ${0.5 * base * height}');
-  } else {
-    print('Invalid input. Please enter valid numbers.');
+  }     else
+  {
+    print(' Please enter valid numbers.');
   }
 }
 
